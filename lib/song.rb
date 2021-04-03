4e2f74528f71d247ed621bbe34da5674e88fd78f
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-04-03 22:45:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,8 @@ class Song
   
   def self.find_by_name(name)
     song = self.all.find{ | song | song.name == name }
-    song
     binding.pry
+    song
   end
   
   def self.find_or_create_by_name(name)
